2bca169157e098b55d62177f74ac469ea11f20f1
Javier Rodríguez Valentín <javrova@gmail.com>
2024-03-03 18:17:11 +0100

    02 - Tic-Tac-Toe
    Modal with the Winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -100,6 +100,26 @@ function App() {
           {TURNS.O}
         </Square>
        </section>
+
+       {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {winner === false ? 'Empate' : 'Ganó: '}
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+       }
     </main>
    
   )
