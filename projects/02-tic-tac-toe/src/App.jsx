3f575803126d309fe924b0ae5a588140cc4e2665
Javier Rodríguez Valentín <javrova@gmail.com>
2024-03-03 18:09:23 +0100

    02 - Tic-Tac-Toe
    Stop the game when there is a winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -21,21 +21,56 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner,setWinner] = useState(null) //null no hay ganador, false hay empate
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras para saber si alguien ganó
+    for (const combo of WINNER_COMBOS){
+      const[a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] 
+      ){
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
   
   const updateBoard = (index) => {
-    //no actualizamos esta posición si ya tiene algo
-    if(board[index]) return
+    //no actualizamos esta posición si ya tiene algo y tampoxo actializamos el board si hay un ganador
+    if(board[index] || winner) return
 
+    //Actualizar el tablero
     /* los valores de las props deben ser inmutables no debemos cambiar sus valores por eso creamos una nueva constante donde almacenamos el nuevo valor */
     const newBoard = [...board] /* spread and rest operator */
     newBoard[index] = turn
     setBoard(newBoard)
-
+    
+    //Cambiamos el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //Revisamos si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner){
+      setWinner(newWinner) //la actualización de los estados en REACT son asíncronos
+    }
   } 
 
   return (
